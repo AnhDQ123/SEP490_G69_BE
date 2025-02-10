3717ac060749031f0074c024c27abf83f743ce8c
htbad <ht.badao@gmail.com>
2025-02-10 10:42:33 +0700

    Package Create

---
+++ src/main/java/org/example/ffb_be/FfbBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.ffb_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FfbBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FfbBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ffb_be/FfbBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ffb_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FfbBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
