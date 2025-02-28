b5c208f9355f564c8968b31b4488928b0dc3361c
AnhDQ <ht.badao@gmail.com>
2025-02-28 11:12:20 +0700

    Update Account feature

--- src/main/java/org/ffb_be/repository/ProfileRepository.java
+++ src/main/java/org/ffb_be/repository/ProfileRepository.java
@@ -13,5 +13,8 @@ public interface ProfileRepository extends JpaRepository<Profile, Long> {
     @Query("SELECT new org.ffb_be.dto.auth.ProfileDto.ProfileDTO(p.id, p.name, p.address, p.avatar) " +
             "FROM Profile p WHERE p.user.id = :userId")
     Optional<ProfileDTO> findByUserId( Long userId);
+        @Query("FROM Profile p WHERE p.user.id = :id")
+    Optional<Profile> findByUserId2(Long id);
+
 
 }
