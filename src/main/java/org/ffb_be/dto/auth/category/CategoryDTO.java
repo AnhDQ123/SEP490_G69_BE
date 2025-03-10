84b3e4af7e6a19b588a41561f00f1a5df5ac629b
AnhDQ <ht.badao@gmail.com>
2025-03-10 11:28:48 +0700

    Update Product feature

---
+++ src/main/java/org/ffb_be/dto/auth/category/CategoryDTO.java
@@ -0,0 +1,16 @@
+package org.ffb_be.dto.auth.category;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryDTO {
+    private Long id;
+    private String name;
+
+
+}
+
