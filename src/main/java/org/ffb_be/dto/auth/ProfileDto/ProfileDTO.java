e523e83af2b21a68760adafffa2f48e1b7b86ebc
AnhDQ <ht.badao@gmail.com>
2025-03-12 09:18:58 +0700

    Update account feature

--- src/main/java/org/ffb_be/dto/auth/ProfileDto/ProfileDTO.java
+++ src/main/java/org/ffb_be/dto/auth/ProfileDto/ProfileDTO.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
@@ -12,4 +15,13 @@ public class ProfileDTO {
     private String name;
     private String address;
     private String avatar;
+    private LocalDateTime createdAt;;
+    private LocalDate dob;
+    private String phone;
+    private String tax_code;
+    private String citizenIDNumber;
+    private String citizenIDCardFront;
+    private String citizenIDCardBack;
+    private LocalDate citizenIDExpiredDate;
+
 }
