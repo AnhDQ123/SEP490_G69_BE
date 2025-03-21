5d2444017b3186357bf4230df3fba4d2da110c7c
NguyenT <nguyenthe171052@fpt.edu.vn>
2025-03-21 10:19:04 +0700

    active user function

--- src/main/java/org/ffb_be/controller/EmailController.java
+++ src/main/java/org/ffb_be/controller/EmailController.java
@@ -1,7 +1,6 @@
 package org.ffb_be.controller;
 
 import org.ffb_be.service.user.EmailService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
--- src/main/java/org/ffb_be/service/user/EmailService.java
+++ src/main/java/org/ffb_be/service/user/EmailService.java
@@ -1,6 +1,5 @@
 package org.ffb_be.service.user;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -8,7 +7,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmailService {
 
-    @Autowired
     private JavaMailSender mailSender;
 
     public void sendEmail(String to, String subject, String body) {
