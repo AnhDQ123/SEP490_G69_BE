42d5aa6d2ea2aa3c8f34b2ad2401f6b10ac47271
NguyenT <nguyenthe171052@fpt.edu.vn>
2025-03-30 22:59:37 +0700

    bug fix

--- src/main/java/org/ffb_be/utils/enums/ConfigCategory.java
+++ src/main/java/org/ffb_be/utils/enums/ConfigCategory.java
@@ -1,7 +1,9 @@
 package org.ffb_be.utils.enums;
 
 public enum ConfigCategory {
-    REPORT_REASON,
+    REPORT_PRODUCT_REASON,
+    REPORT_SHOP_REASON,
+    REPORT_BLOG_REASON,
     ORDER_CANCEL_REASON,
     ORDER_DECLINE_REASON,
     RETURN_REASON
