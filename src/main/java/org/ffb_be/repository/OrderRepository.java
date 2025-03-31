24b24c488e13a8822dcf661f21b287a12f8491d9
AnhDQ <ht.badao@gmail.com>
2025-03-31 23:13:36 +0700

    Update

--- src/main/java/org/ffb_be/repository/OrderRepository.java
+++ src/main/java/org/ffb_be/repository/OrderRepository.java
@@ -97,7 +97,7 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Object[]> countOrdersByStatusAndYear( String status,
                                                LocalDateTime startDate,
                                                LocalDateTime endDate);
-    @Query("SELECT oi.product.id, oi.product.name, SUM(oi.quantity) AS totalQuantity, SUM(oi.quantity * oi.unitPrice) AS totalValue " +
+    @Query("SELECT oi.product.id, oi.product.name, SUM(oi.quantity) AS totalQuantity,  oi.unitPrice AS totalValue " +
             "FROM OrderItem oi " +
             "JOIN oi.order o " +
             "WHERE o.createdAt >= :startDate " +
@@ -105,7 +105,7 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
             "GROUP BY oi.product.id, oi.product.name " +
             "ORDER BY totalQuantity DESC")
     List<Object[]> findTopSellingProductsToday(LocalDateTime startDate, LocalDateTime endDate, Pageable pageable);
-    @Query("SELECT oi.product.id, oi.product.name, SUM(oi.quantity) as totalQuantity,SUM(oi.quantity * oi.unitPrice) AS totalValue " +
+    @Query("SELECT oi.product.id, oi.product.name, SUM(oi.quantity) as totalQuantity, oi.unitPrice AS totalValue " +
             "FROM OrderItem oi " +
             "JOIN oi.order o " +
             "WHERE FUNCTION('YEAR', o.createdAt) = FUNCTION('YEAR', CURRENT_DATE) " +  // Lọc theo năm hiện tại
@@ -114,7 +114,7 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
             "ORDER BY totalQuantity DESC")
     List<Object[]> findTopSellingProductsThisMonth(Pageable pageable);
 
-    @Query("SELECT oi.product.id, oi.product.name, SUM(oi.quantity) as totalQuantity,SUM(oi.quantity * oi.unitPrice) AS totalValue " +
+    @Query("SELECT oi.product.id, oi.product.name, SUM(oi.quantity) as totalQuantity, oi.unitPrice AS totalValue " +
             "FROM OrderItem oi " +
             "JOIN oi.order o " +
             "WHERE FUNCTION('YEAR', o.createdAt) = FUNCTION('YEAR', CURRENT_DATE) " +  // Lọc theo năm hiện tại
