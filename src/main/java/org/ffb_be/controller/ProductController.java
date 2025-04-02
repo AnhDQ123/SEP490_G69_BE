00e05a4d04f7522ab4b3af2f0667a4fa8b317391
AnhDQ <ht.badao@gmail.com>
2025-04-02 22:15:56 +0700

    Update

--- src/main/java/org/ffb_be/controller/ProductController.java
+++ src/main/java/org/ffb_be/controller/ProductController.java
@@ -55,8 +55,8 @@ public class ProductController {
     @PostMapping("/add")
     public ResponseEntity<?> addProduct(@Validated @ModelAttribute() ProductCreateDTO productCreateDTO,
                                          BindingResult bindingResult,
-                                         @RequestParam("avatar") MultipartFile avatar,
-                                         @RequestParam("option")  List<MultipartFile> option) throws IOException {
+                                        MultipartFile avatar,
+                                        List<MultipartFile> option) throws IOException {
         if(bindingResult.hasErrors()) {
             return ResponseEntity.badRequest().body(bindingResult.getAllErrors());
         }
@@ -135,8 +135,8 @@ public class ProductController {
     public ResponseEntity<?> getDrinkByShop(@RequestParam("shopId") Long shopId) {
         return ResponseEntity.ok( productService.findByCategoryByShop(shopId));
     }
-    @GetMapping("/search")
-    public List<ProductResponseDTO> searchProducts(@RequestParam String query) {
-        return productService.searchProducts(query); // Tìm kiếm sản phẩm theo từ khóa
-    }
+//    @GetMapping("/search")
+//    public List<ProductResponseDTO> searchProducts(@RequestParam String query) {
+//        return productService.searchProducts(query); // Tìm kiếm sản phẩm theo từ khóa
+//    }
 }
