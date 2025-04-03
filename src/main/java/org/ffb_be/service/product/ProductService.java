042aaa52e23d401233b34fe356bf88c9bd96b775
AnhDQ <ht.badao@gmail.com>
2025-04-03 13:05:45 +0700

    Update

--- src/main/java/org/ffb_be/service/product/ProductService.java
+++ src/main/java/org/ffb_be/service/product/ProductService.java
@@ -25,7 +25,7 @@ public interface ProductService {
     List<Object[]> findTopSellingProductsThisMonth(Long shopId);
     List<Object[]> findTopSellingProductsThisYear(Long shopId);
     List<ProductResponseDTO> findByCategoryByShop(Long shopId);
-    List<ProductResponseDTO> searchHighlySimilarProducts(String query, int page, int size);
-    String syncAllProducts();
+//    List<ProductResponseDTO> searchHighlySimilarProducts(String query, int page, int size);
+//    String syncAllProducts();
 }
 
