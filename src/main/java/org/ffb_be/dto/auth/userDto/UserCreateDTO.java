75a5f6eb011cc06b5be3632ad62f5cf45965205a
AnhDQ <ht.badao@gmail.com>
2025-04-09 22:48:53 +0700

    Update

--- src/main/java/org/ffb_be/dto/auth/userDto/UserCreateDTO.java
+++ src/main/java/org/ffb_be/dto/auth/userDto/UserCreateDTO.java
@@ -1,15 +1,35 @@
 package org.ffb_be.dto.auth.userDto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Validated
 public class UserCreateDTO {
+    @NotBlank(message = "Phone number is required")
+    @Pattern(
+            regexp = "^0\\d{9}$",
+            message = "Phone number must be 10 digits and start with 0"
+    )
     private String phone;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
     private String email;
+
     private String username;
+
+    @NotBlank(message = "Password is required")
+    @Pattern(
+            regexp = "^(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+            message = "Password must be at least 8 characters long, contain at least one uppercase letter, one digit, and one special character"
+    )
     private String password;
-    private Long roleId;
 }
