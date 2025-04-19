91b338f20db1448ed04303ff631c44a51042e05e
AnhDQ <ht.badao@gmail.com>
2025-04-19 17:53:34 +0700

    Update

--- src/main/java/org/ffb_be/config/security/ElasticsearchConfig.java
+++ src/main/java/org/ffb_be/config/security/ElasticsearchConfig.java
@@ -1,44 +1,57 @@
-//package org.ffb_be.config.security;
-//
-//import co.elastic.clients.elasticsearch.ElasticsearchClient;
-//import co.elastic.clients.json.jackson.JacksonJsonpMapper;
-//import co.elastic.clients.transport.ElasticsearchTransport;
-//import co.elastic.clients.transport.rest_client.RestClientTransport;
-//import org.apache.http.HttpHost;
-//import org.elasticsearch.client.RestClient;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.elasticsearch.client.elc.ElasticsearchTemplate;
-//import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-//import org.springframework.data.elasticsearch.core.convert.ElasticsearchConverter;
-//import org.springframework.data.elasticsearch.core.convert.MappingElasticsearchConverter;
-//import org.springframework.data.elasticsearch.core.mapping.SimpleElasticsearchMappingContext;
-//
-//@Configuration
-//public class ElasticsearchConfig {
-//
-//    @Bean
-//    public RestClient restClient() {
-//        return RestClient.builder(new HttpHost("localhost", 9200)).build();
-//    }
-//
-//    @Bean
-//    public ElasticsearchTransport elasticsearchTransport() {
-//        return new RestClientTransport(restClient(), new JacksonJsonpMapper());
-//    }
-//
-//    @Bean
-//    public ElasticsearchClient elasticsearchClient() {
-//        return new ElasticsearchClient(elasticsearchTransport());
-//    }
-//
-//    @Bean
-//    public ElasticsearchConverter elasticsearchConverter() {
-//        return new MappingElasticsearchConverter(new SimpleElasticsearchMappingContext());
-//    }
-//
-//    @Bean(name = "elasticsearchTemplate")
-//    public ElasticsearchOperations elasticsearchOperations() {
-//        return new ElasticsearchTemplate(elasticsearchClient(), elasticsearchConverter());
-//    }
-//}
+package org.ffb_be.config.security;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+
+import java.net.URI;
+
+@Configuration
+public class ElasticsearchConfig {
+
+    @Value("${spring.elasticsearch.uris}")
+    private String elasticsearchUrl;
+
+    @Value("${spring.elasticsearch.username}")
+    private String username;
+
+    @Value("${spring.elasticsearch.password}")
+    private String password;
+
+    @Bean
+    public ElasticsearchClient elasticsearchClient() {
+        URI uri = URI.create(elasticsearchUrl);
+
+        BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY,
+                new UsernamePasswordCredentials(username, password));
+
+        RestClientBuilder builder = RestClient.builder(
+                        new org.apache.http.HttpHost(uri.getHost(), uri.getPort(), uri.getScheme()))
+                .setHttpClientConfigCallback(httpClientBuilder ->
+                        httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider));
+
+        RestClient restClient = builder.build();
+
+        RestClientTransport transport = new RestClientTransport(
+                restClient, new JacksonJsonpMapper());
+
+        return new ElasticsearchClient(transport);
+    }
+    @Bean(name = "elasticsearchTemplate")
+    public ElasticsearchOperations elasticsearchOperations(ElasticsearchClient client) {
+        return new ElasticsearchTemplate(client);
+    }
+}
