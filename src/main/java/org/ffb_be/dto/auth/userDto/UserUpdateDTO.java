d12066f3a94401155916af39b97a0da15313847d
AnhDQ <ht.badao@gmail.com>
2025-05-03 11:49:45 +0700

    Update

--- src/main/java/org/ffb_be/dto/auth/userDto/UserUpdateDTO.java
+++ src/main/java/org/ffb_be/dto/auth/userDto/UserUpdateDTO.java
@@ -11,10 +11,10 @@ import java.time.LocalDate;
 @Validated
 @Data
 public class UserUpdateDTO {
-    @NotBlank(message = "Account number cannot be blank")
+
     private String accountNumber;
 
-    @NotBlank(message = "Bank code cannot be blank")
+
     private String bankCode;
     private Long id;
 
