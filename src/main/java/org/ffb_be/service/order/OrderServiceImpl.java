121cfbdc19ad5933e8a1d1240addc9d51f2ec99c
AnhDQ <ht.badao@gmail.com>
2025-05-05 13:30:54 +0700

    Update

--- src/main/java/org/ffb_be/service/order/OrderServiceImpl.java
+++ src/main/java/org/ffb_be/service/order/OrderServiceImpl.java
@@ -240,8 +240,8 @@ public class OrderServiceImpl implements OrderService {
         orderDTO.setOrderCode(order.getOrderCode());
         orderDTO.setCreatedAt(order.getCreatedAt());
         orderDTO.setShipperId(order.getShipper() != null ? order.getShipper().getId() : null);
-        orderDTO.setShipperName(userRepository.findById(order.getShipper().getId()).get().getProfile().getName());
-        orderDTO.setShipperPhone(userRepository.findById(order.getShipper().getId()).get().getPhone());
+        orderDTO.setShipperName(order.getShipper() != null ?userRepository.findById(order.getShipper().getId()).get().getProfile().getName(): null);
+        orderDTO.setShipperPhone(order.getShipper() != null ?userRepository.findById(order.getShipper().getId()).get().getPhone(): null);
         orderDTO.setStatus(order.getStatus().toString());
         orderDTO.setPaymentMethodId(order.getPaymentMethod() != null ? order.getPaymentMethod().getId() : null);
         orderDTO.setShipMethodId(order.getDeliveryMethod() != null ? order.getDeliveryMethod().getId() : null);
