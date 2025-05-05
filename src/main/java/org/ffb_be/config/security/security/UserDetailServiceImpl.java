536915b7c6045c78010657aadd9193d32f8d8cb3
AnhDQ <ht.badao@gmail.com>
2025-05-05 13:24:13 +0700

    Update

--- src/main/java/org/ffb_be/config/security/security/UserDetailServiceImpl.java
+++ src/main/java/org/ffb_be/config/security/security/UserDetailServiceImpl.java
@@ -17,7 +17,7 @@ public class UserDetailServiceImpl implements UserDetailsService {
     public UserDetails loadUserByUsername(String phone) throws UsernameNotFoundException {
         User user = userRepository.findByPhone(phone)
                 .orElseThrow(()-> new UsernameNotFoundException("User not found!"));
-    if (user.getStatus().compareTo(Status.INACTIVE)!=0) {
+    if (user.getStatus().compareTo(Status.INACTIVE)==0) {
         throw new UsernameNotFoundException("User has been deactivated!");
     }
         return new UserSecurity(user);
